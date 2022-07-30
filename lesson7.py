6bb66eda60b9b9315fd93faee8d5a82ff4973942
azadbeck <a.uatihcper.z005@gmail.com>
2022-07-30 16:36:56 +0300

    Test

---
+++ lesson7.py
@@ -0,0 +1,107 @@
+
+# import msg 
+# from msg import hello, print_msg
+# from msg import *
+# import msg as test
+# from msg import hello as var1
+# from msg import print_msg as def1
+
+# print(msg.hello)
+# msg.print_msg('hello all')
+
+#
+# print(hello)
+# print_msg('hello work')
+
+# print(test.hello)
+# test.print_msg('hello work')
+#
+#
+# def main():
+# 	print(hello)
+# 	print_msg('hello work')
+#
+#
+# if __name__ == '__main__':
+# 	main()
+
+
+
+
+
+
+# def test():
+# 	return 'hello'
+
+# print(var1)
+# def1('hello from def1')
+
+
+
+# from random import random, randint, randrange, shuffle, choice
+#
+# number = random() * 100
+# print(int(number))
+#
+# rend_num = randint(20, 35)
+# print(rend_num)
+#
+# rand_rang = randrange(2, 10)
+# print(rand_rang)
+#
+# numbers = [i for i in range(1, 11)]
+# print(numbers)
+# shuffle(numbers)
+# print(numbers)
+# print(choice(numbers))
+
+
+import math
+
+
+
+n1 = math.pow(2, 3)
+# print(n1)
+
+
+n2 = 2 ** 3
+# print(n2)
+
+radius = 30
+area = math.pi * math.pow(radius, 2)
+print(area)
+
+number = math.log(10, math.e)
+print(number)
+
+kv = math.sqrt(4)
+print(kv)
+
+# num3 = 2.4
+# num3 = math.floor(nym3)
+# num3 = math.ceil(num3)
+# print(num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ msg.py
@@ -0,0 +1,40 @@
+# import lesson
+
+hello = 'hello all'
+
+def print_msg(text):
+	print(f'Messege: {text}')
+
+
+
+def main():
+	print_msg(hello)
+
+if __name__ == '__main__':
+	print('main')
+else:
+	print(f'this is modul: {__name__}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
